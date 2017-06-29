6625893a6ce70f18f2482133188b9e1b394adeeb
opiumtm <opium_tm@live.ru>
2017-06-29 19:38:32 +0300

    API экспорта архивов

---
+++ Imageboard10/Imageboard10.Core.ModelInterface/Posts/IArchiveCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Windows.Storage;
+
+namespace Imageboard10.Core.ModelInterface.Posts
+{
+    /// <summary>
+    /// Архив.
+    /// </summary>
+    public interface IArchiveCollection : IBoardPostCollection
+    {
+        /// <summary>
+        /// Файлы архива (ключ - строковое представление ссылки на медиа).
+        /// </summary>
+        IDictionary<string, StorageFile> ArchiveFiles { get; }
+    }
+}
---
+++ Imageboard10/Imageboard10.Core.ModelInterface/Posts/Store/IArchiveExportService.cs
@@ -0,0 +1,27 @@
+﻿using Windows.Foundation;
+using Windows.Storage;
+
+namespace Imageboard10.Core.ModelInterface.Posts.Store
+{
+    /// <summary>
+    /// Сервис экспорта и импорта архивов.
+    /// </summary>
+    public interface IArchiveExportService
+    {
+        /// <summary>
+        /// Экспортировать архив. 
+        /// </summary>
+        /// <param name="collection">Коллекция.</param>
+        /// <param name="folder">Директория.</param>
+        /// <param name="fileName">Имя файла.</param>
+        /// <returns>Прогресс (0.0..1.0).</returns>
+        IAsyncActionWithProgress<double> Export(IArchiveCollection collection, StorageFolder folder, string fileName);
+
+        /// <summary>
+        /// Импортировать архив.
+        /// </summary>
+        /// <param name="file">Файл архива.</param>
+        /// <returns>Прогресс (0.0..1.0)</returns>
+        IAsyncOperationWithProgress<IArchiveCollection, double> Import(StorageFile file);
+    }
+}
